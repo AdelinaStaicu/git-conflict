333e019dcf1f4c6756a486e5903a08a75c146546
Dragos Stefanescu <dragos.nicolae.stefanescu@gmail.com>
2021-03-23 19:01:58 +0200

    first commit

--- src/com/unibuc/Main.java
+++ src/com/unibuc/Main.java
@@ -3,6 +3,10 @@ package com.unibuc;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello from the master branch start block");
+
+
+        System.out.println("Hello from the master branch end block");
+
     }
 }

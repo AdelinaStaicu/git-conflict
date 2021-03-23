f2cedafe632b67b97cf6548c78a3f2bbe57fb756
Dragos Stefanescu <dragos.nicolae.stefanescu@gmail.com>
2021-03-23 19:04:06 +0200

    add block code for v1

--- src/com/unibuc/Main.java
+++ src/com/unibuc/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello from the master branch start block");
-
-
+        System.out.println("Hello from the master branch copy version1 start block");
+        System.out.println("Hello from the master branch copy version2 end block");
         System.out.println("Hello from the master branch end block");
 
     }
